418b8ba27d3739608ec6e55b058af91a6ffabac8
trentnixon <trentnixon@gmail.com>
2023-05-13 22:42:35 +1000

    memory tracking

--- updateTask.js
+++ updateTask.js
@@ -1,32 +1,50 @@
 const ClubDetailsController = require("./api/Puppeteer/ClubDetails/GetClubDetails");
 const AssociationDetailsController = require("./api/Puppeteer/AssociationDetails/AssociationDetailsController");
 
-
 /* CLUB */
 async function updateClubDetails(id) {
   try {
+    const intervalId = trackMemoryUsage();
     const clubController = new ClubDetailsController();
     const result = await clubController.setup(id); // Call the Setup method directly
+    clearInterval(intervalId);
     return result;
   } catch (error) {
+    clearInterval(intervalId);
     console.error(`Error getting Club Details: ${error}`);
     throw error;
-  } 
+  }
 }
 
-
 /* ASSOCIATION */
-
 async function updateAssociationDetails(id) {
   try {
+    const intervalId = trackMemoryUsage();
     const associationController = new AssociationDetailsController();
     const result = await associationController.setup(id);
+    clearInterval(intervalId);
     console.log("ASSOCIATION SYNC UPDATE COMPLETE");
     return result;
   } catch (error) {
+    clearInterval(intervalId);
     console.error(`Error getting Association Details: ${error}`);
     throw error;
   }
 }
 
 module.exports = { updateClubDetails, updateAssociationDetails };
+
+function trackMemoryUsage(interval = 20000) {
+  const intervalId = setInterval(() => {
+    const memoryUsage = process.memoryUsage();
+    const memoryUsageInfo = Object.entries(memoryUsage)
+      .map(([key, value]) => {
+        return `${key}: ${(value / 1024 / 1024).toFixed(2)} MB`;
+      })
+      .join(", ");
+
+    console.log(`Memory Usage: ${memoryUsageInfo}`);
+  }, interval);
+
+  return intervalId;
+}
--- worker.js
+++ worker.js
@@ -11,29 +11,26 @@ Lets Refactor the following class, function, or component, adhere to SOLID princ
 // Schedule a task to run every 5 minutes
 cron.schedule("*/1 * * * *", async () => {
   // need to run a call to STRAPI to find an ID to run
-  startTaskRunner()
- 
+  startTaskRunner();
 });
-
 
 async function startTaskRunner() {
   try {
-
     const getSync = await fetcher("account/sync");
     //{ PATH: 'CLUB', ID: 1 }
     console.log("getSync", getSync);
-    if(getSync.continue === true)
-    {
-        getSync.PATH === "CLUB" 
+    if (getSync.continue === true) {
+      // Start tracking memory usage
+      getSync.PATH === "CLUB"
         ? await updateClubDetails(getSync.ID)
         : await updateAssociationDetails(getSync.ID);
       console.log("Task successfully executed");
+    } else {
+      console.log("No Account to Update");
     }
-    else{
-        console.log("No Account to Update")
-    }
-  
   } catch (error) {
     console.error("Error executing the task:", error);
   }
 }
+
+
