73cef8ddbb460531c57d2e2bdc6f22ff0975b43d
trentnixon <trentnixon@gmail.com>
2023-05-14 10:26:01 +1000

    stop service

--- worker.js
+++ worker.js
@@ -11,7 +11,7 @@ Lets Refactor the following class, function, or component, adhere to SOLID princ
 // Schedule a task to run every 5 minutes
 cron.schedule("*/1 * * * *", async () => {
   // need to run a call to STRAPI to find an ID to run
-  startTaskRunner();
+  //startTaskRunner();
 });
 
 async function startTaskRunner() {
