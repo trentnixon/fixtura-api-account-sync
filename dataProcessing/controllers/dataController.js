db1a3a0504c7076ae969184974355fadcc882330
trentnixon <trentnixon@gmail.com>
2024-11-08 14:01:16 +1100

    test(Teams): test memory load for teams

--- dataProcessing/controllers/dataController.js
+++ dataProcessing/controllers/dataController.js
@@ -45,16 +45,16 @@ class DataController {
 
       // Process and assign competitions
       // last checked: 25/1024
-      await this.ProcessCompetitions(dataObj);
+      /*  await this.ProcessCompetitions(dataObj);
       await this.processingTracker.completeStage("competitions", collectionID);
-      dataObj = await this.reSyncData();
+      dataObj = await this.reSyncData(); */
 
       // Process and assign Teams
       // last checked: 25/1024
-      /*   await this.processingTracker.setCurrentStage("teams", collectionID);
+      await this.processingTracker.setCurrentStage("teams", collectionID);
       await this.ProcessTeams(dataObj);
       await this.processingTracker.completeStage("teams", collectionID);
-      dataObj = await this.reSyncData(); */
+      dataObj = await this.reSyncData();
 
       // Process and assign Games
       /*       await this.processingTracker.setCurrentStage("games", collectionID);
